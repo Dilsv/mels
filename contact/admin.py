573d50fb0f35f8b01a8f5078c96c83bf43fdaead
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-04-14 22:14:07 +0100

    Add admin interface for ContactForms model to manage submissions

--- contact/admin.py
+++ contact/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from .models import ContactForms
 
-# Register your models here.
+
+@admin.register(ContactForms)
+class ContactAdmin(admin.ModelAdmin):
+    """
+    A model admin to allow admin to view contact form submissions
+    """
+
+    list_display = (
+        'name',
+        'email',
+        'subject',
+        'message',
+    )
+    list_filter = ('name', 'email', 'subject',)
+    search_fields = ('name', 'email', 'subject',)
