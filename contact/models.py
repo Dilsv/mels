3eca2704f360614609c08f560f7aabbd6f2323af
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-04-14 22:14:26 +0100

    Add ContactForms model to store contact form submissions

--- contact/models.py
+++ contact/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.core.validators import EmailValidator
 
-# Create your models here.
+
+class ContactForms(models.Model):
+    """
+    A model to store contact form submissions in the database
+    """
+    name = models.CharField(max_length=50)
+    email = models.CharField(validators=[EmailValidator()], max_length=100)
+    subject = models.CharField(max_length=100)
+    message = models.TextField()
+
+    class Meta:
+        verbose_name_plural = "contact forms"
+
+    def __str__(self):
+        return f"{self.name}"
