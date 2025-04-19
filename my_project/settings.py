3cbe312d01026ae1875b6aaffef6d0c083aa3804
Dilsat Varli <dilsatvarli@hotmail.co.uk>
2025-04-19 01:51:00 +0100

    Add WHITENOISE_ROOT and STATIC_ROOT settings for improved static file management

--- my_project/settings.py
+++ my_project/settings.py
@@ -159,6 +159,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = '/static/'
+WHITENOISE_ROOT = BASE_DIR / 'staticfiles' / 'build'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
